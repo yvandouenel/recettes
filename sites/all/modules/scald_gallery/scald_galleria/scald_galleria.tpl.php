f4f6dfd92080eee958b9c2b1febac358815b7c1d
yvandouenel <yvan@coopernet.fr>
2014-11-05 16:57:16 +0100

    All drupal installed added.

---
+++ sites/all/modules/scald_gallery/scald_galleria/scald_galleria.tpl.php
@@ -0,0 +1,6 @@
+<div id="<?php print $galleria_id ?>" style="width: <?php print $options['width']; ?>; height: <?php print $options['height']; ?>">
+  <?php foreach ($items as $item): ?>
+    <?php print $item; ?>
+  <?php endforeach; ?>
+</div>
+
